fbad2caeab5b5e7ee82bb9af0b9a1f1cfeeb47f8
Senmori <thesenmori@gmail.com>
2017-03-26 16:35:09 -0400

    Update license to match Quark's original license.
    Signed-off-by: Senmori <thesenmori@gmail.com>

--- src/main/java/net/senmori/vanillatweaks/VanillaTweaks.java
+++ src/main/java/net/senmori/vanillatweaks/VanillaTweaks.java
@@ -1,13 +1,30 @@
+/**
+ * This class was created by Senmori.
+ * Get the Source Code in github:
+ * https://github.com/Senmori/VanillaTweaks
+ *
+ * VanillaTweaks is Open Source and distributed under the
+ * CC-BY-NC-SA 3.0 License: https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB
+ *
+ * This plugin is based off the Quark mod by Vazkii.
+ * This plugin is not endorsed by Vazkii in any way.
+ * Get the Quark mod here: https://minecraft.curseforge.com/projects/quark
+ * Website: http://quark.vazkii.us/
+ * Source Code: https://github.com/Vazkii/Quark
+ */
 package net.senmori.vanillatweaks;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import java.util.logging.Logger;
 import net.senmori.vanillatweaks.commands.CommandManager;
 import net.senmori.vanillatweaks.controllers.ArmorStandController;
 import net.senmori.vanillatweaks.controllers.ConvertClayController;
+import net.senmori.vanillatweaks.controllers.DyedItemNamesController;
 import net.senmori.vanillatweaks.controllers.GrassConvertController;
 import net.senmori.vanillatweaks.controllers.GrassPathController;
 import net.senmori.vanillatweaks.controllers.GrassSpreadController;
 import net.senmori.vanillatweaks.controllers.MinecartController;
+import net.senmori.vanillatweaks.controllers.SignEditController;
 import net.senmori.vanillatweaks.controllers.StackableItemsController;
 import net.senmori.vanillatweaks.controllers.StonePickaxeController;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -51,6 +68,8 @@ public class VanillaTweaks extends JavaPlugin {
         new StackableItemsController(instance);
         new ConvertClayController(instance);
         new StonePickaxeController(instance);
+        new SignEditController(instance);
+        new DyedItemNamesController(instance);
     }
 
     public TweakConfig getTweakConfig() {
