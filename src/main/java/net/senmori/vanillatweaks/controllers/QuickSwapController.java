e7289a7d880271d0042985646850a8a0b7e53736
Senmori <thesenmori@gmail.com>
2017-03-27 02:05:37 -0400

    Fix QuickSwapController.
    Signed-off-by: Senmori <thesenmori@gmail.com>

--- src/main/java/net/senmori/vanillatweaks/controllers/QuickSwapController.java
+++ src/main/java/net/senmori/vanillatweaks/controllers/QuickSwapController.java
@@ -4,7 +4,7 @@ import net.minecraft.server.v1_11_R1.EntityArmorStand;
 import net.minecraft.server.v1_11_R1.EntityPlayer;
 import net.minecraft.server.v1_11_R1.EnumItemSlot;
 import net.senmori.vanillatweaks.VanillaTweaks;
-import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_11_R1.entity.CraftArmorStand;
 import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_11_R1.inventory.CraftItemStack;
@@ -14,12 +14,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 public class QuickSwapController extends TweakController implements Listener {
 
     public QuickSwapController(VanillaTweaks plugin) {
         super(plugin);
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
@@ -27,26 +29,28 @@ public class QuickSwapController extends TweakController implements Listener {
         if(!getPlugin().getTweakConfig().canQuicklySwap()) return;
         if(event.getRightClicked().getType() != EntityType.ARMOR_STAND) return;
         if(!event.getPlayer().isSneaking()) return;
-        System.out.println("TESTING 123");
-        System.err.println("TESTING 123 ERROR");
         event.setCancelled(true);
+        Player player = event.getPlayer();
         ArmorStand stand = (ArmorStand)event.getRightClicked();
-        swapSlot(event.getPlayer(), stand, EnumItemSlot.HEAD);
-        swapSlot(event.getPlayer(), stand, EnumItemSlot.CHEST);
-        swapSlot(event.getPlayer(), stand, EnumItemSlot.LEGS);
-        swapSlot(event.getPlayer(), stand, EnumItemSlot.FEET);
+
+        swapSlot(player, stand, EnumItemSlot.HEAD);
+        swapSlot(player, stand, EnumItemSlot.CHEST);
+        swapSlot(player, stand, EnumItemSlot.LEGS);
+        swapSlot(player, stand, EnumItemSlot.FEET);
         if(getPlugin().getTweakConfig().canSwapOffhand()) {
-            swapSlot(event.getPlayer(), stand, EnumItemSlot.OFFHAND);
+            swapSlot(player, stand, EnumItemSlot.OFFHAND);
         }
     }
 
     private void swapSlot(Player player, ArmorStand stand, EnumItemSlot slot) {
-        EntityPlayer p = ((CraftPlayer)player).getHandle();
-        EntityArmorStand armorStand = ((CraftArmorStand)stand).getHandle();
-        ItemStack pItem = CraftItemStack.asBukkitCopy(p.getEquipment(slot));
-        ItemStack aItem = CraftItemStack.asBukkitCopy(armorStand.getEquipment(slot));
+        EntityPlayer entPlayer = ((CraftPlayer)player).getHandle();
+        EntityArmorStand armor = ((CraftArmorStand)stand).getHandle();
 
-        p.setEquipment(slot, CraftItemStack.asNMSCopy(aItem));
-        armorStand.setEquipment(slot, CraftItemStack.asNMSCopy(pItem));
+        net.minecraft.server.v1_11_R1.ItemStack playerItem = entPlayer.getEquipment(slot);
+        net.minecraft.server.v1_11_R1.ItemStack armorItem = armor.getEquipment(slot);
+
+        entPlayer.setSlot(slot, armorItem);
+        armor.setSlot(slot, playerItem);
+        System.out.println("Swapped " + playerItem.getName() + " with " + armorItem.getName() + " on " + player.getName());
     }
 }
