2fa82d38d5cb80fb681a23d9d1c5222483be5f0f
Senmori <thesenmori@gmail.com>
2017-03-26 22:13:46 -0400

    Fix BurnZombieTask

--- src/main/java/net/senmori/vanillatweaks/tasks/BurnZombieTask.java
+++ src/main/java/net/senmori/vanillatweaks/tasks/BurnZombieTask.java
@@ -1,11 +1,24 @@
 package net.senmori.vanillatweaks.tasks;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.BlockFace;
+import org.bukkit.craftbukkit.libs.joptsimple.internal.Strings;
+import org.bukkit.craftbukkit.v1_11_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_11_R1.entity.CraftZombie;
+import org.bukkit.craftbukkit.v1_11_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Boat;
 import org.bukkit.entity.Zombie;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class BurnZombieTask extends BukkitRunnable {
+    private static final Random rand = new Random();
 
     final JavaPlugin plugin;
     final World world;
@@ -14,13 +27,44 @@ public class BurnZombieTask extends BukkitRunnable {
         this.plugin = plugin;
         this.world = world;
         this.fireTicks = fireTicks;
-        this.runTaskTimer(plugin, 1L, 6L);
+        this.runTaskTimer(plugin, 1L, 20L);
     }
     @Override
     public void run() {
-        world.getEntitiesByClass(Zombie.class).stream()
+        net.minecraft.server.v1_11_R1.World nmsWorld = ((CraftWorld)world).getHandle();
+        if(world.getTime() < 1000 || world.getTime() > 24000) {
+            return; // ignore all this during nighttime
+        }
+        List<Zombie> zombies = world.getEntitiesByClass(Zombie.class).stream()
                                        .filter(Zombie::isBaby)
+                                       .filter(z -> !z.getWorld().hasStorm())
+                                       .filter(z -> !((CraftZombie)z).getHandle().isInWater())
                                        .filter(z -> z.getWorld().getBlockAt(z.getLocation()).getLightFromSky() == 15)
-                                       .forEach(z -> z.setFireTicks(fireTicks));
+                                       .collect(Collectors.toList());
+
+        zombies.forEach(z -> {
+            boolean flag = true;
+            ItemStack helmet = z.getEquipment().getHelmet();
+
+            if(helmet != null) {
+                if(helmet.getDurability() > 0 && ! helmet.getItemMeta().isUnbreakable()) {
+                    helmet.setDurability((short) ( helmet.getDurability() + rand.nextInt(2) ));
+
+                    if(helmet.getDurability() >= helmet.getType().getMaxDurability()) {
+                        helmet.setType(Material.AIR);
+                    }
+                    flag = false;
+                }
+            }
+
+
+            if(flag) {
+                if(z.getFireTicks() <= 0) {
+                    ((CraftZombie)z).getHandle().setOnFire(1);
+                }
+            }
+        });
+
+        zombies.clear();
     }
 }
