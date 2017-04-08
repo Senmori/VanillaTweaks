72ba090a2fbfe5450fdf4f607dda4bd87218a642
Senmori <thesenmori@gmail.com>
2017-04-08 17:54:03 -0400

    Clean up imports. Registe behaviours directly. Open ItemFrame book after a tick.
    Signed-off-by: Senmori <thesenmori@gmail.com>

--- src/main/java/net/senmori/vanillatweaks/controllers/ItemFrameController.java
+++ src/main/java/net/senmori/vanillatweaks/controllers/ItemFrameController.java
@@ -6,7 +6,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.inventory.ItemStack;
@@ -19,30 +18,33 @@ public class ItemFrameController extends TweakController implements Listener {
         if(REGISTRY == null) {
             REGISTRY = new ItemFrameRegistry(getPlugin());
         }
+        //getRegistry().register(new ItemStack(Material.WRITTEN_BOOK), new WrittenBookBehaviour());
         getPlugin().getServer().getPluginManager().registerEvents(this, getPlugin());
     }
 
 
-    @EventHandler(priority = EventPriority.LOWEST)
+    @EventHandler
     public void onClick(PlayerInteractAtEntityEvent event) {
         if(event.getRightClicked().getType() != EntityType.ITEM_FRAME) return;
+        if(!event.getPlayer().isSneaking()) return;
 
         ItemFrame frame = (ItemFrame)event.getRightClicked();
         if(frame.getItem() == null) {
             return;
         }
 
+        Bukkit.broadcastMessage("Frame Item: " + frame.getItem().getType().name().toLowerCase());
         for(ItemStack key : ItemFrameRegistry.REGISTRY.keySet()) {
             Bukkit.broadcastMessage("Key: " + key.getType().name().toLowerCase());
         }
 
-        Bukkit.broadcastMessage("Item not null");
-        if(getRegistry().isRegistered(frame.getItem())) {
+        if(getRegistry().get(frame.getItem()) != null) {
             Bukkit.broadcastMessage("Test1");
             event.setCancelled(true);
             getRegistry().activate(frame.getItem(), frame, event.getPlayer(), event.getClickedPosition());
             Bukkit.broadcastMessage("Post-activate");
         }
+
     }
 
     private ItemFrameRegistry getRegistry() {
