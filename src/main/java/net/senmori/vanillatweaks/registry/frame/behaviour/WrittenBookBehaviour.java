c049f6d0e8ca5e599dd8f0386ac53d6171672bb4
Senmori <thesenmori@gmail.com>
2017-04-08 19:01:16 -0400

    Redo how WrittenBookBehaviour handles opening a book's gui
    Signed-off-by: Senmori <thesenmori@gmail.com>

--- src/main/java/net/senmori/vanillatweaks/registry/frame/behaviour/WrittenBookBehaviour.java
+++ src/main/java/net/senmori/vanillatweaks/registry/frame/behaviour/WrittenBookBehaviour.java
@@ -1,12 +1,10 @@
 package net.senmori.vanillatweaks.registry.frame.behaviour;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import net.minecraft.server.v1_11_R1.PacketDataSerializer;
-import net.minecraft.server.v1_11_R1.PacketPlayOutCustomPayload;
+import net.minecraft.server.v1_11_R1.EnumHand;
 import net.senmori.vanillatweaks.VanillaTweaks;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_11_R1.inventory.CraftItemStack;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -20,20 +18,19 @@ public class WrittenBookBehaviour implements FrameBehaviour {
         Bukkit.broadcastMessage("whoClicked: " + whoClicked.getName());
         ItemStack book = frame.getItem().clone();
 
+
         int slot = whoClicked.getInventory().getHeldItemSlot();
         ItemStack held = whoClicked.getInventory().getItem(slot);
+        EnumHand hand = whoClicked.getInventory().getItemInMainHand().equals(held) ? EnumHand.MAIN_HAND : EnumHand.OFF_HAND;
         Bukkit.broadcastMessage("HeldItem: " + whoClicked.getInventory().getItem(slot).getType().name().toLowerCase());
 
         whoClicked.getInventory().setItem(slot, book);
-        ByteBuf buf = Unpooled.buffer();
-        buf.setByte(0, (byte)0);
-        buf.writerIndex(1);
+
 
         new BukkitRunnable() {
             @Override
             public void run() {
-                PacketPlayOutCustomPayload packet = new PacketPlayOutCustomPayload("MC|BOpen", new PacketDataSerializer(buf));
-                ((CraftPlayer)whoClicked).getHandle().playerConnection.sendPacket(packet);
+                ((CraftPlayer)whoClicked).getHandle().a(CraftItemStack.asNMSCopy(book), hand);
                 whoClicked.getInventory().setItem(slot, held);
                 Bukkit.broadcastMessage("OldItem: " + held.getType().name().toLowerCase());
             }
