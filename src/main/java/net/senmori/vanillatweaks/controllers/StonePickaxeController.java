37d019572ebe69cbf462d205e49dc80ab266b6ed
Senmori <thesenmori@gmail.com>
2017-04-10 15:10:01 -0400

    Remove Listener. Store ingredients and recipeMap in the class for future use.
    Signed-off-by: Senmori <thesenmori@gmail.com>

--- src/main/java/net/senmori/vanillatweaks/controllers/StonePickaxeController.java
+++ src/main/java/net/senmori/vanillatweaks/controllers/StonePickaxeController.java
@@ -1,52 +1,44 @@
 package net.senmori.vanillatweaks.controllers;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import java.util.List;
+import java.util.Map;
 import net.senmori.vanillatweaks.VanillaTweaks;
 import org.bukkit.Material;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 
-public class StonePickaxeController extends TweakController implements Listener {
-
-    private ItemStack STONE = new ItemStack(Material.STONE, 1, (short)0);
-    private ItemStack GRANITE = new ItemStack(Material.STONE, 1, (short)1);
-    private ItemStack DIORITE = new ItemStack(Material.STONE, 1, (short)3);
-    private ItemStack ANDESITE = new ItemStack(Material.STONE, 1, (short)5);
-
-    private List<ItemStack> ingredients = Lists.newArrayList(STONE, GRANITE, DIORITE, ANDESITE, new ItemStack(Material.FLINT));
-
-    private List<String[]> patterns = Lists.newArrayList(new String[]{"XXX", " # ", " # "}, new String[]{" X ", " # ", " # "},
-                                                         new String[]{"XX ", "X# ", " # "}, new String[]{"XX ", " # ", " # "}, new String[]{" X ", " X ", " # "});
-    private List<Material> recipeRewards = Lists.newArrayList(Material.STONE_PICKAXE, Material.STONE_SPADE, Material.STONE_AXE, Material.STONE_HOE, Material.STONE_SWORD);
-
+public class StonePickaxeController extends TweakController {
+    private final Map<Material, String[]> recipeMap;
+    private final List<ItemStack> ingredients;
     public StonePickaxeController(VanillaTweaks plugin) {
         super(plugin);
 
+        recipeMap = ImmutableMap.<Material,String[]>builder()
+                .put(Material.STONE_PICKAXE, new String[]{"XXX", " # ", " # "})
+                .put(Material.STONE_SPADE, new String[]{" X ", " # ", " # "})
+                .put(Material.STONE_AXE, new String[]{"XX ", "X# ", " # "})
+                .put(Material.STONE_HOE, new String[]{"XX ", " # ", " # "})
+                .put(Material.STONE_SWORD, new String[]{" X ", " X ", " # "})
+                .build();
+        ingredients = ImmutableList.<ItemStack>builder()
+                .add(new ItemStack(Material.STONE, 1, (short)0))
+                .add(new ItemStack(Material.STONE, 1, (short)1))
+                .add(new ItemStack(Material.STONE, 1, (short)3))
+                .add(new ItemStack(Material.STONE, 1, (short)5))
+                .add(new ItemStack(Material.FLINT))
+                .build();
+
         if(getPlugin().getTweakConfig().doAddStoneToolVariants()) {
-            for(int i = 0; i < patterns.size(); i++) {
-                for(ItemStack stack : ingredients) {
-                    ShapedRecipe recipe = new ShapedRecipe(new ItemStack(recipeRewards.get(i)));
-                    recipe.shape(patterns.get(i)[0], patterns.get(i)[1], patterns.get(i)[2]);
+            for(ItemStack stack : ingredients) {
+                recipeMap.keySet().forEach( mat -> {
+                    ShapedRecipe recipe = new ShapedRecipe(new ItemStack(mat));
+                    recipe.shape(recipeMap.get(mat)[0], recipeMap.get(mat)[1], recipeMap.get(mat)[2]);
                     recipe.setIngredient('X', stack.getData());
                     recipe.setIngredient('#', Material.STICK);
                     getPlugin().getServer().addRecipe(recipe);
-                }
-            }
-        }
-    }
-
-
-    @EventHandler
-    public void onCraft(PrepareItemCraftEvent event) {
-        if(event.getRecipe() instanceof ShapedRecipe) {
-            ShapedRecipe recipe = (ShapedRecipe)event.getRecipe();
-
-            for(String[] patterns : patterns) {
-
+                });
             }
         }
     }
