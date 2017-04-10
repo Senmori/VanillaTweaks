4f39cffeead3feb1d69c4174dd6837134e1f6017
Senmori <thesenmori@gmail.com>
2017-04-10 10:06:06 -0400

    Re-do GrassPathController logic. Make player swing arm; use NMS to allow statistics to update. Start work on detecting stone recipes
    Signed-off-by: Senmori <thesenmori@gmail.com>

--- src/main/java/net/senmori/vanillatweaks/controllers/GrassPathController.java
+++ src/main/java/net/senmori/vanillatweaks/controllers/GrassPathController.java
@@ -1,12 +1,23 @@
 package net.senmori.vanillatweaks.controllers;
 
 import com.google.common.collect.ImmutableList;
+import net.minecraft.server.v1_11_R1.BlockPosition;
+import net.minecraft.server.v1_11_R1.EntityPlayer;
+import net.minecraft.server.v1_11_R1.EnumHand;
+import net.minecraft.server.v1_11_R1.PacketPlayOutAnimation;
+import net.minecraft.server.v1_11_R1.SoundCategory;
+import net.minecraft.server.v1_11_R1.SoundEffects;
+import net.minecraft.server.v1_11_R1.WorldServer;
 import net.senmori.vanillatweaks.VanillaTweaks;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_11_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_11_R1.inventory.CraftItemStack;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -15,6 +26,8 @@ import org.bukkit.inventory.ItemStack;
 public final class GrassPathController extends TweakController implements Listener {
     private final ImmutableList<Material> validMaterials = ImmutableList.<Material>builder()
             .add(Material.DIRT).build();
+
+    private final ItemStack DIRT = new ItemStack(Material.DIRT, 1, (short)0);
     public GrassPathController(VanillaTweaks plugin) {
         super(plugin);
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
@@ -28,9 +41,24 @@ public final class GrassPathController extends TweakController implements Listen
         if(!validMaterials.contains(event.getClickedBlock().getType())) return;
         if(!getPlugin().getTweakConfig().canConvertDirt()) return;
 
+        EnumHand hand = event.getHand() == EquipmentSlot.HAND ? EnumHand.MAIN_HAND : EnumHand.OFF_HAND;
+        WorldServer wServer = ((CraftWorld)event.getPlayer().getWorld()).getHandle();
+        EntityPlayer human = ((CraftPlayer)event.getPlayer()).getHandle();
+
+        wServer.getTracker().sendPacketToEntity(human, new PacketPlayOutAnimation(human, hand == EnumHand.MAIN_HAND ? 0 : 3)); // send swing arm packet
+
+        net.minecraft.server.v1_11_R1.World nmsWorld = ((CraftWorld)event.getPlayer().getWorld()).getHandle();
+        BlockPosition pos = new BlockPosition(event.getPlayer().getLocation().getX(),event.getPlayer().getLocation().getY(),event.getPlayer().getLocation().getZ());
+        nmsWorld.a(human, pos, SoundEffects.fy, SoundCategory.BLOCKS, 1.0F, 1.0F); // play appropriate sounds
+
+
         event.getClickedBlock().setType(Material.GRASS_PATH);
-        ItemStack heldItem = event.getPlayer().getInventory().getItem(event.getPlayer().getInventory().getHeldItemSlot());
-        heldItem.setDurability((short) (heldItem.getDurability() + 1));
+
+        net.minecraft.server.v1_11_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(event.getItem());
+       nmsStack.damage(1, human); // apply damage,and increase statistics as appropriate
+
+        int slot = event.getPlayer().getInventory().getHeldItemSlot();
+        event.getPlayer().getInventory().setItem(slot, CraftItemStack.asBukkitCopy(nmsStack));
     }
 
 
--- src/main/java/net/senmori/vanillatweaks/controllers/StonePickaxeController.java
+++ src/main/java/net/senmori/vanillatweaks/controllers/StonePickaxeController.java
@@ -4,33 +4,49 @@ import com.google.common.collect.Lists;
 import java.util.List;
 import net.senmori.vanillatweaks.VanillaTweaks;
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 
-public class StonePickaxeController extends TweakController {
+public class StonePickaxeController extends TweakController implements Listener {
 
     private ItemStack STONE = new ItemStack(Material.STONE, 1, (short)0);
     private ItemStack GRANITE = new ItemStack(Material.STONE, 1, (short)1);
     private ItemStack DIORITE = new ItemStack(Material.STONE, 1, (short)3);
     private ItemStack ANDESITE = new ItemStack(Material.STONE, 1, (short)5);
 
+    private List<ItemStack> ingredients = Lists.newArrayList(STONE, GRANITE, DIORITE, ANDESITE, new ItemStack(Material.FLINT));
+
+    private List<String[]> patterns = Lists.newArrayList(new String[]{"XXX", " # ", " # "}, new String[]{" X ", " # ", " # "},
+                                                         new String[]{"XX ", "X# ", " # "}, new String[]{"XX ", " # ", " # "}, new String[]{" X ", " X ", " # "});
+    private List<Material> recipeRewards = Lists.newArrayList(Material.STONE_PICKAXE, Material.STONE_SPADE, Material.STONE_AXE, Material.STONE_HOE, Material.STONE_SWORD);
+
     public StonePickaxeController(VanillaTweaks plugin) {
         super(plugin);
 
         if(getPlugin().getTweakConfig().doAddStoneToolVariants()) {
-            List<ItemStack> ingredients = Lists.newArrayList(STONE, GRANITE, DIORITE, ANDESITE, new ItemStack(Material.FLINT));
-
-            String[][] patterns = new String[][] {{"XXX", " # ", " # "}, {"X", "#", "#"}, {"XX", "X#", " #"}, {"XX", " #", " #"}, {"X", "X", "#"}};
-            ItemStack[] items = {new ItemStack(Material.STONE_PICKAXE), new ItemStack(Material.STONE_SPADE), new ItemStack(Material.STONE_AXE), new ItemStack(Material.STONE_HOE), new ItemStack(Material.STONE_SWORD)};
-
-            for(int i = 0; i < patterns.length; i++) {
+            for(int i = 0; i < patterns.size(); i++) {
                 for(ItemStack stack : ingredients) {
-                    ShapedRecipe recipe = new ShapedRecipe(items[i]);
-                    recipe.shape(patterns[i][0], patterns[i][1], patterns[i][2]);
+                    ShapedRecipe recipe = new ShapedRecipe(new ItemStack(recipeRewards.get(i)));
+                    recipe.shape(patterns.get(i)[0], patterns.get(i)[1], patterns.get(i)[2]);
                     recipe.setIngredient('X', stack.getData());
                     recipe.setIngredient('#', Material.STICK);
                     getPlugin().getServer().addRecipe(recipe);
                 }
+            }
+        }
+    }
+
+
+    @EventHandler
+    public void onCraft(PrepareItemCraftEvent event) {
+        if(event.getRecipe() instanceof ShapedRecipe) {
+            ShapedRecipe recipe = (ShapedRecipe)event.getRecipe();
+
+            for(String[] patterns : patterns) {
+
             }
         }
     }
